4f7fc6a1036e1b19bcec796b92177790c004f9b9
JINXxg <17785872711@163.com>
2024-04-01 17:55:51 +0800

    the file of python about tuple

---
+++ tuples.py
@@ -0,0 +1,27 @@
+#a little like daba's tuple more effiecte than list
+
+''' use slicing to access the subelement
+a=(1,2,3,4,5,6,7,8)
+b=a[1::]
+print(b)
+'''
+
+''' about the inverse
+mytuple=('a','p','p','l','e')
+mylist=list(mytuple)
+print(mylist)
+mytuple2=tuple(mytuple)
+print(mytuple2)
+'''
+
+''' about the tuple lenth or element's length
+mytuple.index('a')
+mytuple.count('')
+len(mytuple)
+'''
+
+
+''' create detail it can not be change
+mytuple=("Max",21,"beach"),also tuple([element])
+if use mytuple=("MAX") it's a str instead of tuple need add ","
+'''
