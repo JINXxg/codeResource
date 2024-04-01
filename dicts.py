76998c1ddfdc5de43b7da1b79c75617dea6a2d79
JINXxg <17785872711@163.com>
2024-04-01 18:17:40 +0800

    a file of python about dictionary

---
+++ dicts.py
@@ -0,0 +1,19 @@
+#dictionary is the key:value pairs now is concer where it will be use
+mydict={"name":"faker","age":27,"city":"soer"}
+#copy same with tuple 
+''' easy way to del the element's in dictionary
+del mydict["age"]
+'''
+
+''' add the more detaile
+mydict["email"]="faker@champion.com"
+'''
+
+''' to check the key value
+for key in mydict.keys():
+'''   
+print()
+'''here is to access the specify info about my dictionary
+value=mydict["name"]
+print(value)
+'''
