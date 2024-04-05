851338b26b60d1d653073c2c2997c7b6175a6009
JINXxg <17785872711@163.com>
2024-04-05 18:01:55 +0800

    a python file of sets

---
+++ sets.py
@@ -0,0 +1,21 @@
+#unordered, mutable, unduplicate
+mysets=set()
+
+odds={1,3,5,7,9}
+evens={0,2,4,6,8}
+primes={1,2,3,4,5}
+
+
+'''math operate
+u=evens.symmetric_difference(odds)
+evens.union(odds)
+evens.intersection(odds)
+print(u)
+'''
+
+''' basic operate of set
+mysets.add()
+mysets.discard()
+mysets.remove()
+mysets.clear()
+'''
